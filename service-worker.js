676ccd331287fa5bbbf69f2b34e90e374459f438
TechLabs Berlin GitHub Workflow <berlin@techlabs.org>
2020-09-14 14:06:23 +0000

    Add frontend build at 2020-09-14T14:06:23UTC

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ca2ab946cfdfd70e7e5d3f4de3eadbd.js"
+  "/precache-manifest.001546657fb805cb40b2af5414802052.js"
 );
 
 self.addEventListener('message', (event) => {
